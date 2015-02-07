feec7fd484066db898a90f7b43960c65cab72d67
Shogo Iwano <shiwano@gmail.com>
2015-02-07 21:39:55 +0900

    Create TodoIoCEvent

---
+++ Assets/TodoIoC/Scripts/Controllers/TodoIoCEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using strange.extensions.signal.impl;
+
+public class TodoIoCEvent
+{
+    public const string CREATE_TODO = "CREATE_TODO";
+    public const string CREATED_TODO = "CREATED_TODO";
+    public const string FINISH_TODO = "FINISH_TODO";
+    public const string REMOVE_ALL_TODOS = "REMOVE_ALL_TODOS";
+    public const string REMOVED_ALL_TODOS = "REMOVE_ALL_TODOS";
+}
