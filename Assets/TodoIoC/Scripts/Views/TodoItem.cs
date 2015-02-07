4d45046ebecb2717fc4434e42a83265fc5df878f
Shogo Iwano <shiwano@gmail.com>
2015-02-08 02:57:30 +0900

    Create TodoItem script

---
+++ Assets/TodoIoC/Scripts/Views/TodoItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TodoItem : MonoBehaviour
+{
+    public Text text;
+    public Image checkmark;
+
+    public TodoModel.Todo Todo { get; private set; }
+    public RectTransform RectTransform { get; private set; }
+
+    private void Awake()
+    {
+        RectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetTodo(TodoModel.Todo todo)
+    {
+        Todo = todo;
+        text.text = todo.Comment;
+        checkmark.gameObject.SetActive(todo.IsFinished);
+    }
+}
