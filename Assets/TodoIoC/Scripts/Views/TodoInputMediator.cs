b80a68d65bfeacdf033c409dd3e9a1c72fb1acab
Shogo Iwano <shiwano@gmail.com>
2015-02-07 21:45:42 +0900

    Create TodoInputView, TodoInputMediator

---
+++ Assets/TodoIoC/Scripts/Views/TodoInputMediator.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.mediation.impl;
+
+public class TodoInputMediator : EventMediator
+{
+}
---
+++ Assets/TodoIoC/Scripts/Views/TodoInputView.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.mediation.impl;
+
+public class TodoInputView : View
+{
+}
