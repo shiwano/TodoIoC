4d7b1f0fa2e6e1028e2f0511100f38eb8edda071
Shogo Iwano <shiwano@gmail.com>
2015-02-07 16:49:05 +0900

    Create TodoModel

---
+++ Assets/TodoIoC/Scripts/Models/TodoModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class TodoModel
+{
+    public class Todo
+    {
+        public string Comment { get; private set; }
+        public bool IsFinished { get; private set; }
+
+        public Todo(string comment)
+        {
+            Comment = comment;
+            IsFinished = false;
+        }
+
+        public void Toggle()
+        {
+            IsFinished = !IsFinished;
+        }
+    }
+
+    public List<Todo> Todos { get; private set; }
+
+    public TodoModel()
+    {
+        Todos = new List<Todo>();
+    }
+
+    public Todo CreateTodo(string comment)
+    {
+        var todo = new Todo(comment);
+        Todos.Add(todo);
+        return todo;
+    }
+
+    public void ClearAllTodos()
+    {
+        Todos.Clear();
+    }
+}
