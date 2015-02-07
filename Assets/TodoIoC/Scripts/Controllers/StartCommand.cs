2cc4932beb32be81111ebe9e91b8d28788c6cc9d
Shogo Iwano <shiwano@gmail.com>
2015-02-08 00:37:34 +0900

    Create commands

---
+++ Assets/TodoIoC/Scripts/Controllers/StartCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using strange.extensions.context.api;
+using strange.extensions.command.impl;
+using strange.extensions.dispatcher.eventdispatcher.impl;
+
+public class StartCommand : EventCommand
+{
+    [Inject(ContextKeys.CONTEXT_VIEW)]
+    public GameObject contextView { get; set; }
+
+    public override void Execute()
+    {
+        Debug.Log("TodoIoC Started");
+        contextView.name += " Started";
+    }
+}
