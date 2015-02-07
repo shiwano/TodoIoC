d5fc3f62bcb3ff97235394e6bb06288a2461b6f0
Shogo Iwano <shiwano@gmail.com>
2015-02-08 02:26:38 +0900

    Create TodoScrollView, TodoScrollMediator

---
+++ Assets/TodoIoC/Scripts/Views/TodoScrollView.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using strange.extensions.dispatcher.eventdispatcher.api;
+using strange.extensions.mediation.impl;
+
+public class TodoScrollView : View
+{
+}
